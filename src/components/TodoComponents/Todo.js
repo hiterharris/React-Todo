73ee8ab0049a0055231b768dd3c9b08cb638e45e
Hiter Harris <hiterharris@comcast.net>
2020-01-13 19:04:19 -0500

    removed unused code

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -8,7 +8,7 @@ class Todo extends React.Component {
             className={`${this.props.item.completed ? "complete" : "incomplete"}`}
             onClick={() => this.props.toggleComplete(this.props.item.id)}
             >
-            <h2 className='todo-item'>{this.props.item.task}</h2>
+            <h2>{this.props.item.task}</h2>
         </div>
     );
   }
